1c4b6af407561f7d8fa23f7c6ab953aa08958d50
Riya Srivastava <riyazerobrokage@gmail.com>
2024-08-06 17:04:09 +0530

    Code updated

--- app/Http/Controllers/MetaDescripConroller.php
+++ app/Http/Controllers/MetaDescripConroller.php
@@ -11,38 +11,23 @@ class MetaDescripConroller extends Controller
     {
         $descriptions = MetaDescription::get();
         $hasDescription = $descriptions->isNotEmpty();
-       
         return view('backend.meta.description', compact('descriptions'));
     }
 
     public function store(Request $request)
     {
-        // $request->validate([
-        //     'desc' => 'required|string|min:10|max:255',
-        // ]);
-
         $validator = Validator::make($request->all(), [
          'desc' => 'required|string|min:10|max:255',
         ]);
-
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-
-
         $desc = new MetaDescription();
         $desc->description = $request->desc;
 
         $desc->save();
         session()->flash('success', 'Submitted Successfully');
-
-        // session()->flash('alert-type', 'success');
-
-        // Return redirect response
-
         return response()->json(['redirect' => url()->previous()]);
-
-
     }
 
     public function update(Request $request, $id)
@@ -50,18 +35,15 @@ class MetaDescripConroller extends Controller
         $validator = Validator::make($request->all(), [
             'description' => 'required|string|min:10|max:255',
            ]);
-   
+
            if ($validator->fails()) {
                return response()->json(['errors' => $validator->errors()], 422);
            }
-        // dd( $request->input('desc'));
         $description = MetaDescription::findOrFail($id);
         $description->description = $request->input('description');
 
         $description->save();
         session()->flash('success', 'Submitted Successfully');
         return response()->json(['redirect' => url()->previous()]);
-
-        // return redirect()->back()->with('success', 'Description updated successfully.');
     }
 }

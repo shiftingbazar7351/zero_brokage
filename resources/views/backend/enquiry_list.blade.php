100cfcc8ff21bcfac6dcf77ec9a37d32df3b045f
Riya Srivastava <riyazerobrokage@gmail.com>
2024-08-09 13:00:47 +0530

    code updated

--- resources/views/backend/enquiry_list.blade.php
+++ resources/views/backend/enquiry_list.blade.php
@@ -142,69 +142,6 @@
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
                     </form>
-                    {{-- <form id="enquiryForm" action="{{ route('enquiry.store') }}" method="POST">
-                        @csrf
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="category" class="form-label">Category</label>
-                                    <select class="form-control" id="category" name="category">
-                                        <option value="" disabled selected>Select Category</option>
-                                        @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    <div class="text-danger category-error"></div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="subcategory" class="form-label">Subcategory</label>
-                                    <select class="form-control" id="subcategory" name="subcategory_id">
-                                        <option value="" disabled selected>Select Subcategory</option>
-                                    </select>
-                                    <div class="text-danger subcategory_id-error"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="move_from_origin" class="form-label">Location</label>
-                                    <input type="text" id="move_from_origin" name="move_from_origin" class="form-control"
-                                        placeholder="Enter your location">
-                                    <div class="text-danger move_from_origin-error"></div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="date_time" class="form-label">Date and Time</label>
-                                    <input type="datetime-local" id="date_time" name="date_time" class="form-control">
-                                    <div class="text-danger date_time-error"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" id="name" name="name" class="form-control"
-                                        placeholder="Enter your name">
-                                    <div class="text-danger name-error"></div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" id="email" name="email" class="form-control"
-                                        placeholder="Enter your email address">
-                                    <div class="text-danger email-error"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="mobile_number" class="form-label">Mobile Number</label>
-                                    <input type="text" id="mobile_number" name="mobile_number" class="form-control"
-                                        placeholder="Enter your mobile number">
-                                    <div class="text-danger mobile_number-error"></div>
-                                </div>
-                            </div>
-                            <div class="text-end">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </div>
-                    </form> --}}
-
                 </div>
             </div>
         </div>
@@ -243,41 +180,6 @@
             });
 
             // for store the data
-            // $('#enquiryForm').on('submit', function(event) {
-            //     event.preventDefault();
-            //     var formData = $(this).serialize();
-            //     var url = $(this).attr('action');
-
-            //     $.ajax({
-            //         url: url,
-            //         method: 'POST',
-            //         data: formData,
-            //         success: function(response) {
-            //             if (response.success) {
-            //                 $('#addEnquiryModal').modal('hide');
-            //                 window.location.reload(); // Refresh the page if successful
-            //             } else {
-            //                 // Clear previous errors
-            //                 $('.text-danger').text('');
-
-            //                 // Show new errors
-            //                 $.each(response.errors, function(key, value) {
-            //                     $('.' + key + '-error').text(value[0]);
-            //                 });
-            //             }
-            //         },
-            //         error: function(xhr) {
-            //             var errors = xhr.responseJSON.errors;
-            //             // Clear previous errors
-            //             $('.text-danger').text('');
-
-            //             $.each(errors, function(key, value) {
-            //                 $('.' + key + '-error').text(value[0]);
-            //             });
-            //         }
-            //     });
-            // });
-
             $(document).ready(function() {
                 $('#enquiryForm').on('submit', function(event) {
                     event.preventDefault(); // Prevent the default form submission
@@ -311,15 +213,11 @@
                         },
                         error: function(xhr) {
                             var errors = xhr.responseJSON.errors;
-
-                            // Clear previous errors
                             $('.text-danger').text('');
 
                             $.each(errors, function(key, value) {
                                 var errorElement = $('.' + key + '-error');
                                 errorElement.text(value[0]);
-
-                                // Remove the error message after 3 seconds
                                 setTimeout(function() {
                                     errorElement.text('');
                                 }, 3000);

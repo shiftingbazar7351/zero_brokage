2e00a207e480cec4b33059c061e279bac565a945
vikas <vikas.zerobrokage@gmail.com>
2024-08-13 16:10:26 +0530

    updated code

--- resources/views/backend/service-detail/edit.blade.php
+++ resources/views/backend/service-detail/edit.blade.php
@@ -86,33 +86,6 @@
             });
         });
 
-        $(document).ready(function() {
-            $('#category').on('change', function() {
-                var categoryId = $(this).val();
-                if (categoryId) {
-                    $.ajax({
-                        url: '/fetch-subcategory/' + categoryId, // Adjusted URL based on route
-                        type: 'POST',
-                        data: {
-                            _token: '{{ csrf_token() }}' // Include CSRF token for security
-                        },
-                        success: function(response) {
-                            if (response.status === 1) {
-                                var subcategory = response.data;
-                                $('#subcategory').find('option').remove(); // Clear existing options
-                                var options = '<option value="">Select Subcategory</option>'; // Default option
-                                $.each(subcategory, function(key, subcateg) {
-                                    options += "<option value='" + subcateg.id + "'>" + subcateg.name + "</option>";
-                                });
-                                $('#subcategory').append(options);
-                            }
-                        }
-                    });
-                } else {
-                    $('#subcategory').find('option').remove(); // Clear options if no state is selected
-                    $('#subcategory').append('<option value="">Select Subcategory</option>');
-                }
-            });
-        });
+
     </script>
 @endsection

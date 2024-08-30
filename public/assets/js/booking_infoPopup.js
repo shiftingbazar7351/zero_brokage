5e686c6fa40fdb2362a5c655c6a86534f9b28135
riya <riyazerobrokage@gmal.com>
2024-08-30 21:11:27 +0530

    u[dates

--- public/assets/js/booking_infoPopup.js
+++ public/assets/js/booking_infoPopup.js
@@ -1,62 +1,67 @@
 $(document).ready(function() {
     // Open the popup when .btn-book is clicked
 
-    $(".book-Now-btn").click(function() {
-        $("#myPopup-booking1").addClass("show");
-        resetBookingDetails();
+    $('.book-Now-btn').click(function() {
+        $('#myPopup-booking1').addClass('show');
+        resetBookingDetails()
     });
 
     // close the popup
 
-    $("#closePopup-booking1").click(function() {
+    $('#closePopup-booking1').click(function() {
         // alert("dfsaj");
-        $("#myPopup-booking1").removeClass("show");
+        $('#myPopup-booking1').removeClass('show');
     });
 
-    $("#saveChanges-booking1").click(function() {
-        if (document.getElementById("phoneNumberInput-booking").value == "") {
 
-        } else {
-            $("#myPopup-booking").addClass("show");
-            resetBookingDetails();
-        }
+    $('#saveChanges-booking1').click(function() {
+        $('#myPopup-booking').addClass('show');
+        resetBookingDetails()
     });
 
-    $("#saveChanges-booking").click(function() {
-
-        $("#myPopup2-booking").addClass("show");
-        $("#myPopup-booking").removeClass("show");
-        $("#myPopup-booking1").removeClass("show");
+    $('#saveChanges-booking').click(function() {
+        $('#myPopup2-booking').addClass('show');
+        $('#myPopup-booking').removeClass('show');
+        $('#myPopup-booking1').removeClass('show');
         startCountdownBooking(60);
     });
 
-    $("#closePopup-booking").click(function() {
-        $("#myPopup-booking").removeClass("show");
-        $("#myPopup-booking1").removeClass("show");
+    $('#closePopup-booking').click(function() {
+        $('#myPopup-booking').removeClass('show');
+        $('#myPopup-booking1').removeClass('show');
     });
 
-    $("#closePopup2-booking").click(function() {
-        $("#myPopup2-booking").removeClass("show");
-        $("#myPopup-booking1").removeClass("show");
+
+    $('#closePopup2-booking').click(function() {
+        $('#myPopup2-booking').removeClass('show');
+        $('#myPopup-booking1').removeClass('show');
+
     });
 
-    $("#editnumber-booking").click(function() {
-        $("#myPopup2-booking").removeClass("show");
-        $("#myPopup-booking").removeClass("show");
-        $("#myPopup-booking1").addClass("show");
+
+    $('#editnumber-booking').click(function() {
+        $('#myPopup2-booking').removeClass('show');
+        $('#myPopup-booking').removeClass('show');
+        $('#myPopup-booking1').addClass('show');
     });
 
-    // $("#verify-otp-booking").click(function () {
-    //     alert("otp verified Succesfully");
-    // });
+    $('#verify-otp-booking').click(function() {
+        alert("otp verified Succesfully");
+    });
+
+
+
 });
+
 
 resendOtpTextBooking.onclick = function() {
     startCountdownBooking(60);
-};
+}
 otpOnWhatsappBooking.onclick = function() {
-    startCountdownBooking(60);
-};
+    startCountdownBooking(60)
+}
+
+
 
 //  let interval;
 function startCountdownBooking(seconds) {
@@ -79,15 +84,22 @@ function startCountdownBooking(seconds) {
     }, 1000);
 }
 
-function resetBookingDetails() {
-    document.getElementById("res-booking1").innerHTML = "";
-    document.getElementById("checkbox-login-booking").checked = false;
 
-    const inputdetails = document.querySelectorAll(".input-detailss");
-    inputdetails.forEach((inputdetail) => {
-        inputdetail.value = "";
+
+
+
+
+function resetBookingDetails() {
+    document.getElementById('res-booking1').innerHTML = '';
+    document.getElementById('checkbox-login-booking').checked = false;
+
+    const inputdetails = document.querySelectorAll('.input-details');
+    inputdetails.forEach(inputdetail => {
+        inputdetail.value = '';
     });
+
 }
+
 
 //    function validateNumBooking(elem){
 //     document.getElementById("res-booking").style.color = "red";
@@ -110,34 +122,25 @@ function resetBookingDetails() {
 //     }
 // }
 
+
 function validateNumBookingg(elemm) {
     document.getElementById("res-booking1").style.color = "red";
     if (isNaN(elemm.value)) {
-        document.getElementById("res-booking1").innerText =
-            "Please enter number only";
-        document
-            .getElementById("saveChanges-booking1")
-            .classList.add("unclickable");
+        document.getElementById("res-booking1").innerText = "Please enter number only";
+        document.getElementById("saveChanges-booking1").classList.add("unclickable");
     } else {
         document.getElementById("res-booking1").innerText = "";
-        document
-            .getElementById("saveChanges-booking1")
-            .classList.remove("unclickable");
+        document.getElementById("saveChanges-booking1").classList.remove("unclickable");
 
         if (elemm.value.length < 10) {
-            document.getElementById("res-booking1").innerText =
-                "Enter 10 digit number";
-            document
-                .getElementById("saveChanges-booking1")
-                .classList.add("unclickable");
+            document.getElementById("res-booking1").innerText = "Enter 10 digit number";
+            document.getElementById("saveChanges-booking1").classList.add("unclickable");
         }
 
         if (elemm.value[0] == 0) {
             document.getElementById("res-booking1").innerText =
                 "First character should not be zero";
-            document
-                .getElementById("saveChanges-booking1")
-                .classList.add("unclickable");
+            document.getElementById("saveChanges-booking1").classList.add("unclickable");
         }
     }
 }

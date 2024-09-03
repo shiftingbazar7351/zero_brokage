a49bfb9ea3475372b7c9ec7f24bd3780ebb5f6d5
Riya Srivastava <riyazerobrokage@gmail.com>
2024-09-03 11:22:39 +0530

    code updated

--- resources/views/frontend/service-in-india.blade.php
+++ resources/views/frontend/service-in-india.blade.php
@@ -284,7 +284,7 @@
                                 <a href="{{ route('services-in-india', $city->name ?? '') }}" class="uppercase">
                                     <h4>{{ $subcategory->slug ?? '' }} {{ strtoupper($city->name) }}</h4>
                                 </a>
-                                <p>{{ Str::limit($vendors->description, 300, '...') }}</p>
+                                <p>{!! Str::limit($vendors->description, 300, '') !!}</p>
                             </div>
                         </div>
                     @endforeach

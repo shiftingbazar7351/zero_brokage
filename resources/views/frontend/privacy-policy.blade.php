e2d51bd98fe6ab2b6deab9078329d243b32f997c
Ghyoor Qasim <ghyoorzerobrokage@gmail.com>
2024-09-05 14:37:21 +0530

    update

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -16,15 +16,16 @@
     </div>
 </div>
 
+
 <div class="content">
     <div class="container">
         <div class="row">
 
             <div class="col-md-12">
                 <div class="terms-content privacy-cont">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc  ing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad   minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commo  do consequat. Duis aute irure dolor in
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
                         sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
                         est laborum.</p>

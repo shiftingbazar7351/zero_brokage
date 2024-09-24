f4c66d974df399884cb7cf0fa9c295001ae6f34b
Riya Srivastava <riyazerobrokage@gmail.com>
2024-09-24 10:20:57 +0530

    updated code

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -12,7 +12,7 @@
                     <h2 class="breadcrumb-title">Privacy Policy</h2>
                     <nav aria-label="breadcrumb" class="page-breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Privacy Policy</li>
                         </ol>
                     </nav>

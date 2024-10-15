418b105b0300d3629c0eec5a8622f44ec92b3a3f
vikas <vikas.zerobrokage@gmail.com>
2024-10-15 11:18:40 +0530

    s

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-print_R(22222);
-die;
+
 Route::get('/pricing-details', function () {
     return view('frontend.pricing');
 })->name('pricing');

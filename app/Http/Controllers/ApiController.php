f6b7274245bfdbdb7489fe3bef0117d46ce3adc3
vikas <vikas.zerobrokage@gmail.com>
2024-10-16 16:30:42 +0530

    s

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -441,7 +441,6 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 // $response = curl_exec($ch);
 
-
             // // $ch = curl_init();
             // curl_setopt($ch, CURLOPT_URL, $url);
             // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
